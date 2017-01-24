9f20aca0693c843e309a84a32823e4869a8496f3
ITLC <1740104885@qq.com>
2017-01-24 14:06:15 +0800

    底部RadioGroup

--- app/src/main/java/com/atguigu/mobileplayer02/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer02/MainActivity.java
@@ -2,12 +2,16 @@ package com.atguigu.mobileplayer02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RadioGroup rg_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg_main = (RadioGroup)findViewById(R.id.rg_main);
+
+        rg_main.check(R.id.rb_local_vido);
     }
 }
